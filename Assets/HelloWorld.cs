becc4354eac01fe8f643a8c847e1ff585413c229
TheMadDabber <stevenashleydawe@gmail.com>
2023-02-17 21:52:33 -0400

    Initial commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("All You Base Are Belong To Me!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
